33d67e3a6853fb47d103950f66d3dff3826a155d
Claudia M. A <claumartal@gmail.com>
2022-01-28 12:45:34 +0100

    Create vigenere.py

---
+++ vigenere.py
@@ -0,0 +1,37 @@
+# pylint: disable=invalid-name
+from pydoc import plain
+import sys
+
+alphabet = 'abcdefghijklmnopqrstuvwxyzåäö'
+plaintext = "varsågod with spaces"
+key = "hej"
+
+key = key.lower()
+plaintext = plaintext.lower()
+n = len(plaintext)
+
+full_key = ''
+cyphertext = ''
+
+# Extend key (repeating it) to cover plaintext of length n
+for i in plaintext[1::len(key)]:
+    for j in key:
+        full_key += j
+
+for i in range(n):
+
+    # If current character is a space, add a space to cyphertext
+    if (plaintext[i] == ' '):
+        cyphertext += ' '
+    # Otherwise, perform shift
+    else:
+        x = alphabet.find(plaintext[i])
+        k = alphabet.find(full_key[i])
+
+        x = x + k
+        if (x > 28):
+            x = x % 29
+
+        cyphertext += alphabet[x]    
+    
+print(cyphertext)
