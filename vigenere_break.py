d19dcaaa4b4c92d03f5b6d3bb2719562d7d7f457
Claudia M. A <claumartal@gmail.com>
2022-02-10 13:28:15 +0100

    Update vigenere_break.py

--- vigenere_break.py
+++ vigenere_break.py
@@ -1,28 +1,29 @@
 # pylint: disable=invalid-name
 import sys
 from math import sqrt
-
 alphabet = 'abcdefghijklmnopqrstuvwxyzåäö'
-# Swedish alphabet frequency table obtained from:
-# http://practicalcryptography.com/cryptanalysis/letter-frequencies-various-languages/swedish-letter-frequencies/
-alphabet_frequencies = {'a': 10.04, 'b': 1.31, 'c': 1.71, 'd': 4.90, 'e': 9.85, 'f': 1.81, 'g': 3.44, 'h': 2.85,
-                        'i': 5.01, 'j': 0.90, 'k': 3.24, 'l': 4.81, 'm': 3.55, 'n': 8.45, 'o': 4.06, 'p': 1.57,
-                        'q': 0.01, 'r': 7.88, 's': 5.32, 't': 8.89, 'u': 1.86, 'v': 2.55, 'w': 0.14, 'x': 0.11,
-                        'y': 0.71, 'z': 0.04, 'å': 1.66, 'ä': 2.10, 'ö': 1.50}
-
+alphabet_frequencies = {'a': 9.98, 'b': 1.36, 'c': 1.68, 'd': 4.92, 'e': 9.76, 'f':
+1.81, 'g': 3.44, 'h': 2.85,
+                        'i': 5.21, 'j': 0.88, 'k': 3.24, 'l': 4.81, 'm': 3.55, 'n':
+8.35, 'o': 4.08, 'p': 1.45,
+                        'q': 0.02, 'r': 7.88, 's': 5.22, 't': 8.85, 'u': 1.86, 'v':
+2.55, 'w': 0.14, 'x': 0.13,
+                        'y': 0.68, 'z': 0.04, 'å': 1.63, 'ä': 2.08, 'ö': 1.55}
 MIN_KEY_LENGTH = 3
 MAX_KEY_LENGTH = 16
 
+
 def findFactors(n):
     """
-    Returns a list containing the factors of n between MIN_KEY_LENGTH and MAX_KEY_LENGTH
+    Returns a list containing the factors of n between MIN_KEY_LENGTH and 
+MAX_KEY_LENGTH
     """
     factors = []
     for i in range(MIN_KEY_LENGTH, MAX_KEY_LENGTH+1):
         if n % i == 0:
             factors.append(i)
     return factors
-
+  
 def repeatedBigrams(ciphertext):
     """
     Returns a list containing all the possible key lengths,
@@ -31,7 +32,6 @@ def repeatedBigrams(ciphertext):
     """
     bigrams = {}
     all_factors = []
-
     n = len(ciphertext)
     for i in range(0,n-1):
         first_bigram = ciphertext[i] + ciphertext[i+1]
@@ -45,7 +45,7 @@ def repeatedBigrams(ciphertext):
                 for x in factors:
                     all_factors.append(x)
     return all_factors
-
+  
 def sortKeyLengths(bigram_factors):
     """
     Given a list of factors bigram_factors, returns a sorted list
@@ -59,17 +59,16 @@ def sortKeyLengths(bigram_factors):
             key_length_dict[x] = 1
         else:
             key_length_dict[x] += 1
-
-    key_length_list = sorted(key_length_dict.items(), key=lambda values: values[1], reverse=True)
+    key_length_list = sorted(key_length_dict.items(), key=lambda values: values[1],
+reverse=True)
     return key_length_list
-
+  
 def getFrequency(text):
     """
     Given a text, returns a list containing all the letters in the
     Swedish alphabet and how many times they appear in the text
     """
     freq_dict = {}
-
     # Count letters in text
     # If letter does not appear in the text, add it to the list
     # with value 0
@@ -78,15 +77,13 @@ def getFrequency(text):
             freq_dict[letter] =  1
         else:
             freq_dict[letter] +=  1
-
     for letter in alphabet:
         if letter not in freq_dict:
             freq_dict[letter] = 0
-
     # Convert dict to list and sort letters alphabetically
     freq_list = sorted(freq_dict.items(), reverse=False)
     return freq_list
-
+  
 def tryAttack(ciphertext, key_length):
     """
     Given a ciphertext and a possible key length,
@@ -102,16 +99,14 @@ def tryAttack(ciphertext, key_length):
         for j in ciphertext[i::key_length]:
             current_column.append(j)
         columns.append(current_column)
-
      # Obtain letter frequency by column
     frequencies = []
     print("Letter count by column: ")
     for column in columns:
-        print("Column ", columns.index(column))
+        #print("Column ", columns.index(column))
         column_freq = getFrequency(column)
         frequencies.append(column_freq)
-        print(column_freq, "\n")
-
+        #print(column_freq, "\n")
     # Guess most likely key for each column
     key = ''
     for column in columns:
@@ -124,29 +119,29 @@ def tryAttack(ciphertext, key_length):
             # Letter count in current column decrypted with i:
             freq_cipher_shifted = getFrequency(shifted_text) 
             chi_sum = 0
-
             for shifted_letter in freq_cipher_shifted:
-                # Actual letter count of current letter in the decrypted text:
+                # Actual count of current letter in the decrypted text:
                 freq_decrypted = shifted_letter[1]
-                # Expected letter count of current letter in the decrypted text:
-                freq_swedish = (alphabet_frequencies[shifted_letter[0]]/100)*len(column)
-                # Compute chi-squared for current letter/key and add to total sum for this key:
+                # Expected count of current letter in the decrypted text:
+                freq_swedish = 
+(alphabet_frequencies[shifted_letter[0]]/100)*len(column)
+                # Compute chi-squared for current letter/key and add to total sum 
+for this key:
                 chi_sum += (sqrt(abs(freq_decrypted-freq_swedish)))/freq_swedish
-
-            # If the chi-squared statistic for this column deciphered with the current key
-            # is lower than the previous values, this key is more likely to be correct
+            # If the chi-squared statistic for this column deciphered with the 
+current key
+            # is lower than the previous values, this key is more likely to be 
+correct
             # for this column
             if (chi_sum<key_sum or key_sum==-1):
                 key_sum = chi_sum
                 current_key = i
-
         # Add the guessed key for the current column
         # to the complete key
         key += current_key
-
     # Return complete key
     return key
-
+  
 def decrypt(ciphertext, key):
     """
     Given a ciphertext and a key, shifts each letter in the text
@@ -155,13 +150,11 @@ def decrypt(ciphertext, key):
     """
     plaintext = ''
     full_key = ''
-
     # Repeat the key until it covers the full
     # length of the ciphertext
     for i in ciphertext[0::len(key)]:
         for j in key:
             full_key += j
-
     # Shift each letter in the ciphertext by its corresponding
     # key
     for i in range(len(ciphertext)):
@@ -171,19 +164,83 @@ def decrypt(ciphertext, key):
         x = x % 29
         plaintext += alphabet[x]    
     return plaintext
-
+  
 def printTextSeparated(text, space):
     """
     Prints a given text separating it into chunks of size space
     """
-    print("\nThe resulting decrypted text is: ")
+    print("\nThe resulting text is: ")
     for i in enumerate(text):
         print(i[1], end= '')
         if (i[0]+1)%space==0:
             print(end = ' ')
     print("\n")
-
-
+    
+def tryKeys(ciphertext, key_length_list):
+    """
+    Iterates over the key_length_list and attempts to break the
+    ciphertext with each possible key length.
+    Prompts the user to choose which key lengths to try.
+    """
+    key=''
+    for i in key_length_list:
+        key_length = i[0]
+        answer = input("\nTry to hack with key length " + str(key_length) + "? 
+(Y/N/Exit) ").lower()
+        if answer in ('y', 'yes', ''):
+            key = tryAttack(ciphertext, key_length) 
+            print("\nKey obtained: " + key)
+            plaintext = decrypt(ciphertext, key)
+            printTextSeparated(plaintext, key_length)
+        elif answer in ('exit', 'e'):
+            return
+        else:
+            pass
+                       
+def tryAttackMultiple():
+    """
+    Attempts to break a cipher when the input is multiple
+    ciphertexts that use the same key.
+    Reads ciphertexts from terminal.
+    Concatenates texts (after removing parts that would not
+    be long enough for the key) and attempts to break them as if
+    they were one.
+    """
+    ciphertexts = []
+    key_length_list = []
+    n = int(input("Number of ciphertexts to analyse: "))
+    for i in range(n):
+        text = input("Input ciphertext "+ str(i) + ": ")
+        ciphertexts.append(text)
+        key_length_list += (repeatedBigrams(text))
+    key_length_list = sortKeyLengths(key_length_list)
+    for key_length in key_length_list:
+        cut_texts = ''
+        # For a given key, trims each text so that the length of the resulting
+        # text is a multiple of the key length
+        for text in ciphertexts:
+            split_at = 0
+            for i in range(len(text)):
+                j = i+key_length[0]
+                if (j%key_length[0]==0 and j<len(text)):
+                    split_at = j
+            cut_texts += text[0:split_at]
+        # Attempt to break cipher as if it was one single ciphertext
+        answer = input("\nTry to hack with key length " + str(key_length[0]) + "? 
+(Y/N/Exit) ").lower()
+        if answer in ('y', 'yes', ''):
+            key = tryAttack(cut_texts, key_length[0]) 
+            print("\nKey obtained: " + key)
+            plaintext = decrypt(cut_texts, key)
+            printTextSeparated(cut_texts, key_length[0])
+        elif answer in ('exit', 'e'):
+            return
+        else:
+            pass
+    for text in ciphertexts:
+        print("Trying to decrypt ciphertext #", ciphertexts.index(text))
+        tryKeys(text,key_length_list)
+                       
 def main():
     """
     Main function.
@@ -192,37 +249,25 @@ def main():
     """
     option = input("Choose option: \n"
                 "1. Try to guess key and break a Vigenere cipher, \n"
-                "2. Decrypt text with a given key \n")
+                "2. Decrypt text with a given key \n"
+                "3. Try to guess key for multiple texts that use the same key\n"
+                "4. Split a text into chunks of a given size\n")
     if option=='1':
         ciphertext = input("Input ciphertext: ")
         ciphertext = ciphertext.lower()
-
         print("\nUsing the Kasiski method to guess key length...")
-
         # Kasiski Method
         ## Step 1: Find repeated bigrams and spacing between them
         all_factors = repeatedBigrams(ciphertext)
-
-        ## Step 2: Sort key lengths by how likely they are to be the correct key length
+        ## Step 2: Sort key lengths by how likely they are to be the correct key 
+length
         key_length_list = sortKeyLengths(all_factors)
-        print("List of possible key lengths and their number of appearances (length, appearances):")
+        print("List of possible key lengths and their number of appearances 
+(length, appearances):")
         print(key_length_list)
-
-        ## Step 3: Try key lengths starting by the most possible one
-        key = ''
-        for i in key_length_list:
-            key_length = i[0]
-            answer = input("\nTry to hack with key length " + str(key_length) + "? (Y/N/Exit) ").lower()
-            if answer in ('y', 'yes', ''):
-                key = tryAttack(ciphertext, key_length) 
-                print("\nKey obtained: " + key)
-                plaintext = decrypt(ciphertext, key)
-                printTextSeparated(plaintext, key_length)
-            elif answer in ('exit', 'e'):
-                sys.exit()
-            else:
-                pass
-
+        ## Step 3: Try key lengths starting by the most possible one, to guess 
+correct key
+        tryKeys(ciphertext, key_length_list)
     if option=='2':
         ciphertext = input("Input ciphertext: ")
         key = input("\nInput key: (input E to exit) ").lower()
@@ -230,8 +275,9 @@ def main():
             plaintext = decrypt(ciphertext, key)
             printTextSeparated(plaintext, len(key))
             key = input("\nInput key: (input E to exit) ").lower()
+    if option=='3':
+        tryAttackMultiple()
     else:
         pass
-
 if __name__ == '__main__':
     main()
